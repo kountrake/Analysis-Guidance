cf727b79df889543e62d38e012c1817d3cf4a68f
Bastien Naessens <bastien.naessens.etu@univ-lille.fr>
2021-01-31 16:27:44 +0100

    Implémentation:
    RegisterValidator:
        -Réalisation des différentes méthodes permettant la validation des tests

--- src/Project/Validator/RegisterValidator.php
+++ src/Project/Validator/RegisterValidator.php
@@ -7,13 +7,22 @@ class RegisterValidator
 
     public function isValidName(string $name): bool
     {
+        $res = filter_var($name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z][a-z]+$/")));
+        return  $res ? true : false;
     }
 
     public function isValidEmail(string $email): bool
     {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) ? true : false;
     }
 
     public function isValidPassword(string $password): bool
     {
+        //One upper, on lower, one number, maybe one special
+        // $regexp = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&.;:,ùµ£çéè'\)\(\\\{\}=+°~²]{8,}$/"
+        // One upper, one lower, one number, no special
+        $regexp = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/";
+        $res = filter_var($password, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=> $regexp)));
+        return $res ? true : false;
     }
 }
