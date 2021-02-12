1d5c898520cdb112534af9e8bfc0e14fad02a7d4
david <lucas.david.etu@univ-lille.fr>
2021-02-12 16:09:58 +0100

    ajout page score

---
+++ src/Project/Controller/ScoreController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Project\Controller;
+
+use GuzzleHttp\Psr7\Response;
+
+class ScoreController extends Controller
+{
+    public function index()
+    {
+        $this->view('score');
+    }
+}
