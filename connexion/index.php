1f98369ba26c9da52b133c1b1fb82f71a2e3a36a
Sow Papalaye <papalaye.sow.etu@univ-lille.fr>
2021-02-15 20:23:26 +0100

    Partie Connexion

---
+++ connexion/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="author" content="NoS1gnal"/>
+            <link rel="stylesheet" type="text/css" media="screen" href="../style.css" />
+        
+            <title>Connexion</title>
+        </head>
+        <body>
+        
+        <div class="login-form">
+             
+            
+            <form action="connexion.php" method="post">
+                <h2 class="text-center">Connexion</h2>       
+                <div class="form-group">
+                    <input type="email" name="email" placeholder="Email" required="required" >
+                </div>
+                <div >
+                    <input type="password" name="password" placeholder="Mot de passe" required="required" >
+                </div>
+                <div >
+                    <button type="submit" >Connexion</button>
+                </div>   
+            </form>
+            
+        </div>
+        
+        </body>
+</html>
