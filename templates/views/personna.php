5435772a7d7d359d737375bb6d51afe18ff19f9f
Bastien Naessens <bastien.naessens.etu@univ-lille.fr>
2021-03-03 17:51:29 +0100

    FIX:     personna.php:         correction erreurs dans les formulaires

--- templates/views/personna.php
+++ templates/views/personna.php
@@ -12,25 +12,33 @@
                         <label class="block text-grey-darker text-sm font-bold mb-2" for="usname">
                             Nom :
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="usname" type="text">
+                        <input name="usname"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="usname" type="text">
                     </div>
                     <div class="mb-4">
                         <label class="block text-grey-darker text-sm font-bold mb-2" for="entantque">
                             Age :
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="entantque" type="text">
+                        <input name="entantque"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="entantque" type="text">
                     </div>
                     <div class="mb-4">
                         <label class="block text-grey-darker text-sm font-bold mb-2" for="jeveux">
-                            Proffession :
+                            Profession :
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="jeveux" type="text">
+                        <input name="jeveux"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="jeveux" type="text">
                     </div>
                     <div class="mb-4">
                         <label class="block text-grey-darker text-sm font-bold mb-2" for="desorte">
                             Situation familiale :
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="desorte" type="text">
+                        <input name="desorte"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="desorte" type="text">
                     </div>
                 </form>
             </div>
@@ -38,76 +46,66 @@
                 <h3 class="text-center underline text-xl">Caractéristiques</h3>
                 <form method="post" class="p-4">
                     <div class="mb-4 w-9/12 float-left">
-                        
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="usname" type="text">
+                        //TODO Il faut un name au champ
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="usname" type="text">
                     </div>
-                    
-                    
-                    
-                <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
-                            Ajouter
-            </button></form>
+                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
+                        Ajouter
+                    </button>
+                </form>
             </div>
             <div class="bg-white float-left w-5/12 pb-72 mt-5">
                 <h3 class="text-center underline text-xl">Objectifs</h3>
                 <form method="post" class="p-4">
                     <div class="mb-4 w-9/12 float-left">
-                        
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="usname" type="text">
+                        //TODO Il faut un name au champ
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="usname" type="text">
                     </div>
-                    
-                    
-                    
-                <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
-                            Ajouter
-            </button></form>
+                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
+                        Ajouter
+                    </button>
+                </form>
             </div>
             <div class="bg-white float-right w-5/12 pb-72 mt-5">
                 <h3 class="text-center underline text-xl">Scénarios</h3>
                 <form method="post" class="p-4">
                     <div class="mb-6 w-9/12 float-left">
-                        
-                        <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker" id="satisfait" type="text">
+                        //TODO Il faut un name au champ
+                        <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker"
+                               id="satisfait" type="text">
                     </div>
                     <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
-                            Ajouter
+                        Ajouter
                     </button>
                 </form>
             </div>
             <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 relative top-full left-10">
-                            Générer
+                Générer
             </button>
         </div>
         <div class="mt-12 mx-2 w-full bg-white rounded">
             <h3 class="text-center underline text-xl">Identité</h3>
             <p class="p-2">Nom :</p>
-                <p class="p-2">Age :</p>
-                <p class="p-2">Proffession :</p>
-                <p class="p-2">Situation familiale :</p>
+            <p class="p-2">Age :</p>
+            <p class="p-2">Profession :</p>
+            <p class="p-2">Situation familiale :</p>
         </div>
         <div class="mt-12 mx-2 w-full bg-white rounded pb-12">
             <h3 class="text-center underline text-xl">Caractéristiques</h3>
-            
-                
-                
-                
-        </div><div class="mt-12 mx-2 w-5/12 bg-white rounded pb-12 float-left mb-24 mr-40">
+        </div>
+        <div class="mt-12 mx-2 w-5/12 bg-white rounded pb-12 float-left mb-24 mr-40">
             <h3 class="text-center underline text-xl">Objectifs</h3>
-            
-                
-                
-                
-        </div><div class="mt-12 mx-2 w-5/12 bg-white rounded pb-12 float-right">
+        </div>
+        <div class="mt-12 mx-2 w-5/12 bg-white rounded pb-12 float-right">
             <h3 class="text-center underline text-xl">Scénarios</h3>
-            
-                
-                
-                
-        </div><button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right mt-4 ml-8">
-                            Suivant
+        </div>
+        <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right mt-4 ml-8">
+            Suivant
         </button>
         <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right mt-4">
-                            Télécharger
+            Télécharger
         </button>
     </div>
 </div>
