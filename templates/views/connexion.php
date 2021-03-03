e8828d7aaa3197d8e0aad2219ab8cde0b6c8c975
Sow Papalaye <papalaye.sow.etu@univ-lille.fr>
2021-03-03 01:05:10 +0100

    Fix #5:la redirection à modifier juste dans l'emplacement du fichier

---
+++ templates/views/connexion.php
@@ -0,0 +1,29 @@
+<?php 
+    session_start();
+    require_once 'config.php';
+
+    if(isset($_POST['email']) && isset($_POST['password']))
+    {
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+
+        $check = $bdd->prepare('SELECT pseudo, email, password FROM papa WHERE email = ?');
+        $check->execute(array($email));
+        $data = $check->fetch();
+        $row = $check->rowCount();
+
+        if($row == 1)
+        {
+            if(filter_var($email, FILTER_VALIDATE_EMAIL))
+            {
+                
+                if(password_verify($password, $data['password']))
+                {
+                   $_SESSION['user'] = $data['email'];
+                    header('Location: landing.php'); /*redirection à modifier */
+                    die();
+                }else{ header('Location: index.php?login_err=password'); die(); }
+            }else{ header('Location: index.php?login_err=email'); die(); }
+        }else{ header('Location: index.php?login_err=already'); die(); }
+    }
+
