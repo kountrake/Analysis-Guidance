d4af25c50d035ce080f51a0bdb68eeae31446c6a
Bastien Naessens <bastien.naessens.etu@univ-lille.fr>
2021-03-05 06:54:36 +0100

    DESIGN:     help.php:         Changement de la présentation de l'exemple d'une user story

--- templates/views/help.php
+++ templates/views/help.php
@@ -7,30 +7,47 @@
         <h1 class="w-full bg-white text-center text-4xl font-bold underline p-2">Personna</h1>
         <div class="flex justify-center">
             <p class="mt-5 rounded p-2 w-2/3 bg-white">
-                Un Persona est une personne fictive qui représente un groupe cible de profils utilisateurs répondant à un cas d’usage particulier. Le but va être de l’enrichir pour le rendre le plus “vivant” possible. Pour l’enrichir, il faudra réutiliser les caractéristiques des utilisateurs obtenus lors des interviews préalables. Ces caractéristiques vont ensuite permettre de guider les choix sur les fonctionnalités, la conception et aider à prendre des décisions concernant le produit.
+                Un Persona est une personne fictive qui représente un groupe cible de profils utilisateurs répondant à
+                un cas d’usage particulier. Le but va être de l’enrichir pour le rendre le plus “vivant” possible. Pour
+                l’enrichir, il faudra réutiliser les caractéristiques des utilisateurs obtenus lors des interviews
+                préalables. Ces caractéristiques vont ensuite permettre de guider les choix sur les fonctionnalités, la
+                conception et aider à prendre des décisions concernant le produit.
             </p>
         </div>
     </div>
     <div class=" w-full mt-10 ">
         <h1 class="w-full bg-white text-center text-4xl font-bold underline p-2">User-Story</h1>
         <div class="flex justify-center">
-            <p class="mt-5 rounded p-2 w-2/3 bg-white">
-                Une histoire-utilisateur est l'énoncé de ce que cherche à faire un acteur et dans quel but. Il suffit de le formaliser de la façon suivante:
-En tant que < acteur >, 
-je veux < une certaine fonctionnalité >,
-de sorte que < objectif à atteindre décrivant la plus-value, besoin réel >. 
-Je peux vérifier cette histoire en : 
-• < liste des conditions d'acceptation >.
-
-            </p>
+            <div class="mt-5 rounded p-2 w-2/3 bg-white flex flex-col">
+                <p>
+                    Une histoire-utilisateur est l'énoncé de ce que cherche à faire un acteur et dans quel but. Il
+                    suffit de le formaliser de la façon suivante:
+                </p>
+                <ul class="m-4">
+                    <li>En tant que < acteur >,</li>
+                    <li>Je veux < une certaine fonctionnalité >,</li>
+                    <li>De sorte que < objectif à atteindre décrivant la plus-value, besoin réel >.</li>
+                    <li>Je peux vérifier cette histoire en :
+                        <ul class="pl-4 text-sm">
+                            <li class="ml-4">< Condition d'acceptation n°1></li>
+                            <li class="ml-4">< Condition d'acceptation n°2></li>
+                            <li class="ml-4">< Condition d'acceptation n°3></li>
+                            <li class="ml-4">...</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class=" w-full mt-10 ">
         <h1 class="w-full bg-white text-center text-4xl font-bold underline p-2">Story-Map</h1>
         <div class="flex justify-center">
             <p class="mt-5 rounded p-2 w-2/3 bg-white">
-                Cette représentation permet de mettre en perspective le développement du produit avec le parcours utilisateur. 
-Elle comporte deux axes, à l'horizontal, on trouve le flot de narration suivant les étapes du processus-métier. Tandis qu’à l’horizontal on trouve les besoins par objectifs, du plus prioritaire au moins prioritaires.
+                Cette représentation permet de mettre en perspective le développement du produit avec le parcours
+                utilisateur.
+                Elle comporte deux axes, à l'horizontal, on trouve le flot de narration suivant les étapes du
+                processus-métier. Tandis qu’à l’horizontal on trouve les besoins par objectifs, du plus prioritaire au
+                moins prioritaires.
             </p>
         </div>
     </div>
