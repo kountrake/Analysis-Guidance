ef9ae8d9998378136a57de4f9236e7199e7d98f3
Bastien Naessens <bastien.naessens.etu@univ-lille.fr>
2021-03-11 18:44:28 +0100

    Fix #48 #49: - Ajout du bouton précédent              - Lien mis à jour du bouton fin de projet

--- templates/views/score.php
+++ templates/views/score.php
@@ -1,32 +1,37 @@
-<div class="flex justify-center w-full">
-    <form class="bg-white w-2/3 mt-10 p-4 rounded" method="post">
-        <h1 class="text-center text-4xl font-bold underline mb-20">Score</h1>
-            <div class="mb-4">
-                <p class="p-2">
-                    Persona : note/25
-                </p>
-                <p class="p-2">
-                    User Story : note/25
-                </p>
-                <p class="p-2">
-                    StoryMap : note/25
-                </p>
-                <p class="p-2">
-                    Matrice : note/25
-                </p>
-                <p class="p-2 text-center text-xl">
-                    Score final : note/100
-                </p>
-                <div class="flex items-center justify-between pt-2">
-                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
-                        Aller à mes projets
-                    </button>
-                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
-                        Fin du projet 
-                    </button>
-                    
-            </div>
+<div class="w-full">
+    <div class="flex justify-center bg-white w-full my-10 p-4">
+        <h1 class="text-center text-4xl font-bold underline">Score</h1>
+    </div>
 
-            </div>
-    </form>
+    <div class="flex justify-center mb-4">
+        <div class="w-2/3 bg-white rounded py-10 px-20">
+            <p class="p-2">
+                Persona : note/25
+            </p>
+            <p class="p-2">
+                User Story : note/25
+            </p>
+            <p class="p-2">
+                StoryMap : note/25
+            </p>
+            <p class="p-2">
+                Matrice : note/25
+            </p>
+            <p class="p-2 text-center text-xl">
+                Score final : note/100
+            </p>
+        </div>
+    </div>
+
+    <div class="flex items-center justify-around pt-2">
+        <a href="/matrice"
+           class="block bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
+            Précédent
+        </a>
+        <a href="/myprojects"
+           class="block bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
+            Fin du projet
+        </a>
+    </div>
+
 </div>
