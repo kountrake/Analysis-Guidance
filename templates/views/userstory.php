2c85d01aaa8e4c50eb43739b939d6d4b20938895
Bastien Naessens <bastien.naessens.etu@univ-lille.fr>
2021-03-11 17:35:23 +0100

    Fix #38 #39 : - Correction du CSS et du HTML               - Mise en place des 3 champs je suis satisfait si               - Ajout du bouton précédent

--- templates/views/userstory.php
+++ templates/views/userstory.php
@@ -3,71 +3,91 @@
         <h1 class="text-center text-4xl font-bold underline">User Story</h1>
     </div>
 
-    <div class="w-full px-8 h-screen">
-        <div class="mt-10 mx-2 w-full h-1/2 mb-20">
-            <div class="bg-white rounded float-left w-5/12">
-                <h3 class="text-center underline text-xl">Expression du besoin</h3>
-                <form method="post" class="p-4">
+    <div class="w-full px-8">
+        <form method="post" class="p-4 w-full">
+            <div class="flex flex-row">
+                <div class="flex flex-col w-1/2 bg-white rounded p-2 mr-4">
+                    <h3 class="text-center underline text-xl">Expression du besoin</h3>
                     <div class="mb-4">
                         <label class="block text-grey-darker text-sm font-bold mb-2" for="entantque">
                             En tant que :
                         </label>
-                        <input name ="entantque" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="entantque" type="text">
+                        <input name="entantque"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="entantque" type="text">
                     </div>
                     <div class="mb-4">
                         <label class="block text-grey-darker text-sm font-bold mb-2" for="jeveux">
                             Je veux :
                         </label>
-                        <input name="jeveux" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="jeveux" type="text">
+                        <input name="jeveux"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="jeveux" type="text">
                     </div>
                     <div class="mb-4">
                         <label class="block text-grey-darker text-sm font-bold mb-2" for="desorte">
                             De sorte que :
                         </label>
-                        <input name="desorte" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="desorte" type="text">
+                        <input name="desorte"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               id="desorte" type="text">
                     </div>
-                    
-                </form>
-            </div>
-            <div class="bg-white rounded mt-10 float-right w-5/12">
-                <h3 class="text-center underline text-xl">Critères de satisfaction</h3>
-                <form method="post" class="p-4">
+                </div>
+                <div class="flex flex-col w-1/2  bg-white rounded p-2">
+                    <h3 class="text-center underline text-xl">Critères de satisfaction</h3>
                     <div class="mb-6">
-                        <label class="block text-grey-darker text-sm font-bold mb-2" for="satisfait">
-                            Je suis satisfait si :
-                        </label>
-                        <input name="satisfait" class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker" id="satisfait" type="text">
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="critere1">Critère
+                                1:</label>
+                            <input name="critere1" class="border shadow rounded w-full py-2 px-3 text-grey-darker"
+                                   id="critere1" type="text">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="critere2">Critère
+                                2:</label>
+                            <input name="critere2" class="border shadow rounded w-full py-2 px-3 text-grey-darker"
+                                   id="critere2" type="text">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="critere3">Critère
+                                3:</label>
+                            <input name="critere3" class="border shadow rounded w-full py-2 px-3 text-grey-darker"
+                                   id="critere3" type="text">
+                        </div>
                     </div>
-                    <button type='submit' class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
-                            Ajouter
-                    </button>
-                </form>
+                </div>
             </div>
-            <button type='submit' class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 relative top-full left-16">
-                            Générer
-            </button>
-        </div>
-        
-        <div class="mt-10 mx-2 w-full bg-white rounded">
+            <div class="flex justify-center mt-4">
+                <button type='submit'
+                        class="block bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
+                    Générer
+                </button>
+            </div>
+        </form>
+
+        <div class="mt-10 bg-white rounded">
             <h3 class="text-center underline text-xl">Us-00X - Nom</h3>
             <p class="p-2">
-                    En tant que
-                </p>
-                <p class="p-2">
-                    Je veux :
-                </p>
-                <p class="p-2">
-                    De sorte que :
-                </p>
-                <p class="p-2">
-                    Je suis satisfait si :
-                </p>
+                En tant que
+            </p>
+            <p class="p-2">
+                Je veux :
+            </p>
+            <p class="p-2">
+                De sorte que :
+            </p>
+            <p class="p-2">
+                Je suis satisfait si :
+            </p>
         </div>
-        <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right ml-10 mt-4">
-                            Suivant
-        </button>
-        <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right mt-4">
-                            Télécharger
-        </button>
+        <div class="flex flex-row justify-around mt-4">
+            <a href="/personna"
+               class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">Précédent</a>
+            <a href="/download"
+               class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">Télécharger</a>
+            <a href="/storymap"
+               class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">Suivant</a>
+        </div>
+
     </div>
 </div>
