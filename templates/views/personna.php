87d6d502a6ac97c08c2a4bf8ae9e51201ff15f2b
Bastien Naessens <bastien.naessens.etu@univ-lille.fr>
2021-03-11 16:42:50 +0100

    Fix #23 #24 #25 #27 #30 #35 :     persona.php: - Ajout sur le site d'un champ prénom                  - Le bouton suivant fonctionne                  - Le champ caractéristique du site est renommé en description et est un textarea                  - Le champ objectif devient une description des objectifs en un seul paragraphe et est un textarea                  - Suppression des boutons ajouter                  - Correction du CSS

--- templates/views/personna.php
+++ templates/views/personna.php
@@ -3,104 +3,91 @@
         <h1 class="text-center text-4xl font-bold underline">Personna</h1>
     </div>
 
-    <div class="w-full px-8 h-screen">
-        <div class="mt-10 mx-2 w-full h-5/6 mb-20">
-            <div class="bg-white float-left w-5/12 pb-4">
-                <h3 class="text-center underline text-xl">Identité</h3>
-                <form method="post" class="p-4">
-                    <div class="mb-4">
-                        <label class="block text-grey-darker text-sm font-bold mb-2" for="usname">
+    <div class="flex flex-col w-full px-8">
+        <div class="my-4 mx-2">
+            <form method="post">
+                <div class="flex flex-row justify-between">
+                    <div class="flex flex-col justify-around w-1/2 bg-white p-4 mb-4 mr-4">
+                        <h3 class="text-center underline text-xl">Identité</h3>
+                        <label class="text-grey-darker text-sm font-bold mb-2" for="usname">
                             Nom :
                         </label>
-                        <input name="usname"
-                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                        <input name="name"
+                               class="border rounded py-2 px-3 text-grey-darker"
                                id="usname" type="text">
-                    </div>
-                    <div class="mb-4">
-                        <label class="block text-grey-darker text-sm font-bold mb-2" for="entantque">
+                        <label class="text-grey-darker text-sm font-bold mb-2" for="usname">
+                            Prénom :
+                        </label>
+                        <input name="firstname"
+                               class="border rounded py-2 px-3 text-grey-darker"
+                               id="usname" type="text">
+                        <label class="text-grey-darker text-sm font-bold mb-2" for="entantque">
                             Age :
                         </label>
                         <input name="entantque"
-                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               class="border rounded py-2 px-3 text-grey-darker"
                                id="entantque" type="text">
-                    </div>
-                    <div class="mb-4">
-                        <label class="block text-grey-darker text-sm font-bold mb-2" for="jeveux">
+                        <label class="text-grey-darker text-sm font-bold mb-2" for="jeveux">
                             Profession :
                         </label>
                         <input name="jeveux"
-                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                               class="border rounded py-2 px-3 text-grey-darker"
                                id="jeveux" type="text">
                     </div>
-                </form>
-            </div>
-            <div class="bg-white float-right w-5/12 pb-80">
-                <h3 class="text-center underline text-xl">Caractéristiques</h3>
-                <form method="post" class="p-4">
-                    <div class="mb-4 w-9/12 float-left">
-                        <p>//TODO Il faut un name au champ <br>
-                            //TODO Plusieurs caractéristiques ? <br>
-                            // => Comment gérer l'ajout dynamique des multiples caractéristiques ? <br>
-                            // => Comment gérer l'ajout de ces multiples caractéristiques dans la BDD ?</p>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                               id="usname" type="text">
+                    <div class="flex flex-col justify-around w-1/2 bg-white mb-4 p-4">
+                        <label for="description" class="text-center underline text-xl">Description</label>
+                        <textarea class="border rounded w-full py-2 px-3 text-grey-darker" id="usname" maxlength="300"
+                                  rows="9" cols="33"></textarea>
                     </div>
-                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
-                        Ajouter
-                    </button>
-                </form>
-            </div>
-            <div class="bg-white float-left w-5/12 pb-72 mt-5">
-                <h3 class="text-center underline text-xl">Objectifs</h3>
-                <form method="post" class="p-4">
-                    <div class="mb-4 w-9/12 float-left">
-                        //TODO Il faut un name au champ
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                               id="usname" type="text">
+                </div>
+                <div class="flex flex-row justify-between">
+                    <div class="flex flex-col justify-around w-1/2 bg-white mb-4 p-4 mr-4">
+                        <label for="objectifs" class="text-center underline text-xl mb-2">Objectifs</label>
+                        <textarea class="border rounded w-full py-2 px-3 text-grey-darker" maxlength="300" rows="5"
+                                  cols="33" id="usname"></textarea>
                     </div>
-                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
-                        Ajouter
-                    </button>
-                </form>
-            </div>
-            <div class="bg-white float-right w-5/12 pb-72 mt-5">
-                <h3 class="text-center underline text-xl">Scénarios</h3>
-                <form method="post" class="p-4">
-                    <div class="mb-6 w-9/12 float-left">
-                        //TODO Il faut un name au champ
-                        <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker"
-                               id="satisfait" type="text">
+                    <div class="flex flex-col justify-around w-1/2 bg-white mb-4 p-4">
+                        <label for="scenarios" class="text-center underline text-xl mb-2">Scénarios</label>
+                        <textarea class="border border-red rounded w-full py-2 px-3 text-grey-darker" maxlength="300"
+                                  rows="5" cols="33"></textarea>
                     </div>
-                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">
-                        Ajouter
+                </div>
+                <div class="flex justify-center">
+                    <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
+                        Générer
                     </button>
-                </form>
-            </div>
-            <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 relative top-full left-10">
-                Générer
-            </button>
+                </div>
+            </form>
         </div>
-        <div class="mt-12 mx-2 w-full bg-white rounded">
+
+        <div class="w-full bg-white rounded mb-4 p-4">
             <h3 class="text-center underline text-xl">Identité</h3>
             <p class="p-2">Nom :</p>
+            <p class="p-2">Prénom :</p>
             <p class="p-2">Age :</p>
             <p class="p-2">Profession :</p>
             <p class="p-2">Situation familiale :</p>
         </div>
-        <div class="mt-12 mx-2 w-full bg-white rounded pb-12">
-            <h3 class="text-center underline text-xl">Caractéristiques</h3>
+        <div class="bg-white rounded mb-4 p-4">
+            <h3 class="text-center underline text-xl">Description</h3>
         </div>
-        <div class="mt-12 mx-2 w-5/12 bg-white rounded pb-12 float-left mb-24 mr-40">
+        <div class="bg-white rounded mb-4 p-4">
             <h3 class="text-center underline text-xl">Objectifs</h3>
         </div>
-        <div class="mt-12 mx-2 w-5/12 bg-white rounded pb-12 float-right">
+        <div class="bg-white rounded mb-4 p-4">
             <h3 class="text-center underline text-xl">Scénarios</h3>
         </div>
-        <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right mt-4 ml-8">
-            Suivant
-        </button>
-        <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right mt-4">
-            Télécharger
-        </button>
+
+        <div class="flex flew-row justify-around mb-4">
+            <a href="/download"
+               class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
+                Télécharger
+            </a>
+            <a href="/userstory"
+               class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600">
+                Suivant
+            </a>
+        </div>
+
     </div>
 </div>
