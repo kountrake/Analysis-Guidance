c83dd7afcf7f99963be872541e48c184c3468b01
alexis inacio <alexis.inacio.etu@univ-lille.fr>
2021-03-19 14:00:28 +0100

    Fix #36 #40

--- templates/views/userstory.php
+++ templates/views/userstory.php
@@ -113,7 +113,7 @@
             <a href="/personna/<?= $projectId ?>"
                class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold
                hover:underline hover:bg-blue-600">Précédent</a>
-            <a href="/download"
+            <a href="/download/userstory/<?= $projectId ?>"
                class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold
                hover:underline hover:bg-blue-600">Télécharger</a>
             <a href="/storymap"
