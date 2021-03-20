7e82b21a812f59b52e015608e5f9f1cd4a8cbec2
david <lucas.david.etu@univ-lille.fr>
2021-03-20 16:51:49 +0100

    ajout css storymap/activite

--- templates/views/storymap/activite.php
+++ templates/views/storymap/activite.php
@@ -3,45 +3,45 @@
         <h1 class="text-center text-4xl font-bold underline">StoryMap - Les activités et leurs stories</h1>
     </div>
 
-    <div class="flex flex-col w-full px-8">
+    <div class="flex flex-col px-8 mt-4 bg-white ml-4 mr-4">
         <div>
-            <p>Dans cette deuxième étape vous allez lier des stories avec des activités</p>
+            <p class="text-center text-xl pt-4">Dans cette deuxième étape vous allez lier des stories avec des activités</p>
         </div>
         <div class="my-4 mx-2">
             <form method="post" action="/storymap/activite/create">
                 <input type="hidden" name="projectId" value="<?= $projectId ?>">
                 <?php $i = 0 ?>
                 <?php foreach ($roles as $role) : ?>
-                    <p><?= $role->role ?></p>
+                    <p class="text-xl"><?= $role->role ?></p>
                     <?php foreach ($activites[$i] as $activite) : ?>
                         <p><?= $activite->activite ?> : </p>
                         <div class="mb-6">
                             <div class="mb-4">
-                                <p class="block text-grey-darker text-sm font-bold mb-2">Story1</p>
+                                <p class="block text-grey-darker text-sm font-bold mb-2 pt-2">Story1</p>
                                 <input name="<?= $role->role . '_' . $activite->activite . '_1' ?>"
                                        class="border shadow rounded w-full py-2 px-3 text-grey-darker"
                                        type="text">
-                                <p class="block text-grey-darker text-sm font-bold mb-2">priorité:</p>
+                                <p class="block text-grey-darker text-sm font-bold mb-2 pt-2">priorité:</p>
                                 <input name="<?= $role->role . '_' . $activite->activite . 'priorite_1' ?>"
                                        class="border shadow rounded w-full py-2 px-3 text-grey-darker"
                                        type="text">
                             </div>
                             <div class="mb-4">
-                                <p class="block text-grey-darker text-sm font-bold mb-2">Story2</p>
+                                <p class="block text-grey-darker text-sm font-bold mb-2 pt-2">Story2</p>
                                 <input name="<?= $role->role . '_' . $activite->activite . '_2' ?>"
                                        class="border shadow rounded w-full py-2 px-3 text-grey-darker"
                                        type="text">
-                                <p class="block text-grey-darker text-sm font-bold mb-2">priorité:</p>
+                                <p class="block text-grey-darker text-sm font-bold mb-2 pt-2">priorité:</p>
                                 <input name="<?= $role->role . '_' . $activite->activite . 'priorite_2' ?>"
                                        class="border shadow rounded w-full py-2 px-3 text-grey-darker"
                                        type="text">
                             </div>
                             <div class="mb-4">
-                                <p class="block text-grey-darker text-sm font-bold mb-2">Story3</p>
+                                <p class="block text-grey-darker text-sm font-bold mb-2 pt-2">Story3</p>
                                 <input name="<?= $role->role . '_' . $activite->activite . '_3' ?>"
                                        class="border shadow rounded w-full py-2 px-3 text-grey-darker"
                                        type="text">
-                                <p class="block text-grey-darker text-sm font-bold mb-2">priorité:</p>
+                                <p class="block text-grey-darker text-sm font-bold mb-2 pt-2">priorité:</p>
                                 <input name="<?= $role->role . '_' . $activite->activite . 'priorite_3' ?>"
                                        class="border shadow rounded w-full py-2 px-3 text-grey-darker"
                                        type="text">
@@ -50,7 +50,7 @@
                     <?php endforeach; ?>
                     <?php $i++ ?>
                 <?php endforeach; ?>
-            <button>Créer</button>
+            <button class="bg-blue-700 rounded border-2 border-blue-800 p-2 text-white text-semi-bold hover:underline hover:bg-blue-600 float-right">Créer</button>
             </form>
         </div>
     </div>
