30f1e0a62850d678c6b98da3600fe28f30d14101
alexis inacio <alexis.inacio.etu@univ-lille.fr>
2021-03-22 16:50:29 +0100

    correction downloadcontroller

--- src/Project/App.php
+++ src/Project/App.php
@@ -97,7 +97,6 @@ class App
         /*
          * Les routes pour le téléchargement
          */
-        $this->router->addRoute('GET', 'download', '/download/:id', 'DownloadController@index');
         $this->router->addRoute('GET', 'download_personna', '/download/personna/:id', 'DownloadController@gethtmlPersonna');
         $this->router->addRoute('GET', 'download_userstory', '/download/userstory/:id', 'DownloadController@gethtmlUserStory');
         $this->router->addRoute('GET', 'download_storymap', '/download/storymap/:id', 'DownloadController@gethtmlStoryMap');
